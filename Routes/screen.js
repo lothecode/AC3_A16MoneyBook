a718ff195df05d21d74e670a7cc9716fe7df55f1
cflo <cflo013@gmail.com>
2020-03-11 17:17:39 +0800

    Add filter by month and rewrite the related code in index.handlebars & screen.js

--- Routes/screen.js
+++ Routes/screen.js
@@ -2,32 +2,44 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../models/record')
 const categories = require('../data/categories.json')
+const months = require('../data/months.json')
 const { authenticated } = require('../config/auth')
 
 // sort by category
-router.get('/:screen', authenticated, (req, res) => {
-  Record.find({ userId: req.user._id })
+router.get('/?', authenticated, (req, res) => {
+  const selectCategory = req.query.cat || ''
+  const selectMonth = req.query.month || ''
+  const findMonth = new RegExp(selectMonth, 'g') // 含有selectMonth的字串, 'g'flag: global search
+  const userId = req.user._id
+  let querys = { userId }
+  let showCategory = '類別 (全部)'
+  let showMonth = '月份 (全部)'
+  if (selectCategory === '' && selectMonth !== '') {
+    querys = { userId, date: { $regex: findMonth } }
+    showMonth = months[selectMonth].month_ch
+  } else if (selectCategory !== '' && selectMonth === '') {
+    querys = { userId, category: selectCategory }
+    showCategory = categories[selectCategory].category_ch
+  } else if (selectCategory !== '' && selectMonth !== '') {
+    querys = { userId, category: selectCategory, date: { $regex: findMonth } }
+    showCategory = categories[selectCategory].category_ch
+    showMonth = months[selectMonth].month_ch
+  }
+
+  Record
+    .find(querys)
     .lean()
     .exec((err, records) => {
-      let selectCategory = req.params.screen
       if (err) return console.error(err)
-      // 依據選擇的類別找出結果
-      if (selectCategory === 'all') {
-        return res.redirect('/records')
-      } else {
-        const results = records.filter(item => {
-          return item.category === selectCategory
-        })
-        // 計算選出來之類別總金額
-        let total = 0
-        results.forEach(item => {
-          total += item.amount
-          item.icon = categories[item.category].icon
-        })
-        // 選擇類別後保留選項在畫面
-        showCat = categories[selectCategory].category_ch
-        return res.render('index', { categories, showCat, records: results, total })
-      }
+      let total = 0
+      records.forEach(item => {
+        total += item.amount
+        item.icon = categories[item.category].icon
+      })
+      // 選擇類別後保留選項在畫面 ??
+
+      return res.render('index', { categories, records, total, selectCategory, selectMonth, showMonth, showCategory })
+
     })
 })
 module.exports = router
