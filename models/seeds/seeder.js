926b9e9ec499e8cc3e1600fe85d7eed8482cec7b
cflo <cflo013@gmail.com>
2020-03-12 12:09:13 +0800

    Modify seeder.js, and adjust login/register templates

--- models/seeds/seeder.js
+++ models/seeds/seeder.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose')
 const Record = require('../record')
 const member = require('../seeds/namelist.json')
+const bill = require('../seeds/bills')
 const User = require('../user')
 const bcrypt = require('bcryptjs')
 
 mongoose.connect('mongodb://localhost/expense', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }) // 設定連線到 mongoDB的expense資料庫
-
 // mongoose 連線後透過 mongoose.connection 拿到 Connection 的物件
 const db = mongoose.connection
 // 連線異常及成功
 db.on('error', () => { console.log('mongodb error!') })
-
 db.once('open', () => {
   console.log('mongodb connected!')
   let namelist = member.namelist
@@ -18,19 +17,31 @@ db.once('open', () => {
     bcrypt.genSalt(10, (err, salt) =>
       bcrypt.hash(i.password, salt, (err, hash) => {
         const user = new User({
-          name: i.email.slice(0, i.email.indexOf('@')),
+          name: i.name,
           email: i.email,
           password: hash
         })
         user.save((err) => {
           if (err) return console.error(err)
         })
+
+        let list = bill.records
+        for (let mybill of list) {
+          i.select.forEach(id => {
+            if (mybill.id === id) {
+              Record.create({
+                name: mybill.name,
+                category: mybill.category,
+                date: mybill.date,
+                amount: mybill.amount,
+                userId: user._id
+              })
+            }
+          })
+        }
       }
       ))
-
-    //record還沒加
   }
-
 })
 
 console.log('done')
